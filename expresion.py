d1bdd3b0662163681cf7ea386c80ace1b9023b19
bjimenez <bjimenezg07@gmail.com>
2019-04-15 01:35:37 -0500

    Version actualizada de un AFD

--- expresion.py
+++ expresion.py
@@ -1,80 +1,83 @@
 
 """Laboratorio#2"""
 """ El programa debe aceptar la siquiente expresion regular: (a|b)*abb"""
-##########################Inicio del Automata############################
+
+########################## Inicio del Automata ############################
 def estado0():
 	pos = 0
 	cadena = raw_input("Ingrese la primera letra:")
 	for x in cadena:
 		if x == 'a':
-			print ("va al estado1 :")
+			print ("Recorre del Estado0 al Estado1 :")
 			estado1(cadena, pos)
 			break
 
 		elif x == 'b':
-			print ("Continue al estado4:")
-			#funcion estado5
+			print ("Recorre del Estado0 al Estado4 :")
 			estado4(cadena, pos)
 			break
+
 		else:
-			print("Error, cadena no valida")
+			print("Error, cadena no es valida")
 			break
 		pos +=1
 
-##########################Fin del estado0############################
+########################## Fin del Estado0 ############################
 
 def estado1(cadena, pos):
 	inc = pos+1
 	if inc < len(cadena):
 		if cadena[inc] == 'a':
-			print ("lee estado1:")
+			print ("Retorna del Estado1 al Estado1 :")
 			estado1(cadena, inc)
 
 		elif cadena[inc] == 'b':
-		    print("va al estado 2")
+		    print("Recorre del Estado1 al Estado2 :")
 		    estado2(cadena, inc)
 		else:
-			print("Error, cadena no valida")
+			print("Error, cadena no es valida")
 	else:
 		print("Error, La cadena no es valida")
 
-##########################Fin del estado1############################
+########################## Fin del Estado1 ############################
 
 def estado2(cadena, pos):
 	inc = pos+1
 	if inc < len(cadena):
 		if cadena[inc] == 'a':
-			print ("retorna al estado1:")
+			print ("Recorre del Estado2 al Estado1 :")
 			estado1(cadena, inc)
 
 		elif cadena[inc] == 'b':
-			print ("Va al estado3:")
+			print ("Recorre del Estado2 al Estado3 :")
 	    		estado3(cadena,inc)
 	    	else:
-			print("Error, cadena no valida")
+			print("Error, cadena no es valida")
 	else:
 		print("Error, La cadena no es valida")
 
-##########################Fin del estado2############################
+########################## Fin del Estado2 ############################
 
 def estado3(cadena, pos):
-	inc = pos
-	if cadena[inc] == 'b':
-		print("Cadena valida.")
+	inc = pos+ 1
+	if inc < len(cadena):
+		if cadena[inc] =='a' or cadena[inc] == 'b':
+			print("Error, La cadena no es valida")
+		
+	elif cadena[pos] == 'b':
+		print(" la Cadena es valida.")
 
-
-##########################Fin del estado3############################
+########################## Fin del Estado3 ############################
 
 def estado4(cadena, pos):
-	#print ("prueba2:",pos)
 	inc = pos+1
 	if inc < len(cadena):
 		if cadena[inc] == 'a':
-			print ("pasa al estado1:")
+			print ("Recorre del Estado4 al Estado1 :")
 			estado1(cadena, inc)
 
 		elif cadena[inc] == 'b':
-	    		print ("bucle en el estado4:")
+	    		print ("Retorna del Estado4 al Estado4 :")
 	    		estado4(cadena, inc)	
 	    	else:
 			print("Error, cadena no valida")
@@ -82,5 +85,7 @@ def estado4(cadena, pos):
 		print("Error, La cadena no es valida")
 
 estado0()
-##########################Fin del estado4############################
+
+########################## Fin del Estado4 ############################
+
 
